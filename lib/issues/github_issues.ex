77a4b421f120c7eb8b06d83b59a8eeae9a1e604d
Exadra37 <exadra37@gmail.com>
2018-05-02 17:49:18 +0000

    Starting Github issues implementation.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,27 @@
+defmodule Issues.GithubIssues do
+
+  @user_agent [ {"User-Agent", "Elixir dave@pragprog.com"} ]
+
+  # the environment variable is fetched at compile time
+  #@github_url "https://api.github.com"
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body)}
+  end
+
+  def handle_response({_, %{status_code: _, body: body}}) do
+    {:error, Poison.Parser.parse!(body)}
+  end
+
+end
