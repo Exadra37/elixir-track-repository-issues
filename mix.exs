11b078c20186b045b49347285d545c59c7562f4c
Exadra37 <exadra37@gmail.com>
2018-05-10 07:26:23 +0000

    --wip-- [skip ci]

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
@@ -25,4 +26,9 @@ defmodule Issues.MixProject do
       poison:    "~> 2.2",
     ]
   end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
+  end
+
 end
